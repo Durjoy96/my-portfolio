481913fc99c1b9db114a52594b0b81cf93dfddee
Durjoy96 <mddurjoy853@gmail.com>
2025-02-07 18:39:32 +0600

    Add Contact component and include it in the Home page

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,4 +1,5 @@
 import About from "@/components/about/About";
+import Contact from "@/components/contact/Contact";
 import Hero from "@/components/heroSection/Hero";
 import Projects from "@/components/projects/Projects";
 import Skills from "@/components/skills/Skills";
@@ -10,6 +11,7 @@ export default function Home() {
       <About />
       <Skills />
       <Projects />
+      <Contact />
     </>
   );
 }
