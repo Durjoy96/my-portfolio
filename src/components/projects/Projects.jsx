9ab4b0b9a58b3a2fe43a5bfabce98c80597c66f2
Durjoy96 <mddurjoy853@gmail.com>
2025-06-01 11:45:47 +0600

    Refactor Projects component layout and enhance project link interaction

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -4,7 +4,6 @@ import proyojonImg from "../../assets/images/proyojon.png";
 import titlyImg from "../../assets/images/titly.png";
 import parazImg from "../../assets/images/paraz.png";
 import Link from "next/link";
-import { ExternalLink } from "lucide-react";
 
 const Projects = () => {
   const projects = [
@@ -26,34 +25,35 @@ const Projects = () => {
   ];
   return (
     <>
-      <section className="main-container py-12 md:py-20 lg:py-32" id="projects">
-        <div className="">
+      <section className="bg-base-200 py-12 md:py-20 lg:py-32" id="projects">
+        <div className="main-container">
           <Label text="Projects" />
           <h2 className="text-2xl md:text-4xl font-bold text-base-content mt-6">
             Built with Code, Fueled by Passion
           </h2>
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mt-8 md:mt-12">
             {projects.map((project, idx) => (
-              <div
-                key={idx}
-                className="bg-base-300 p-6 rounded-lg border border-white/40"
-              >
-                <Image
-                  className="rounded-lg"
-                  src={project.image}
-                  alt={project.title}
-                />
-                <h3 className="mt-6 text-2xl font-medium text-base-content">
-                  {project.title}
-                </h3>
-                <Link
-                  href={project.url}
-                  className="mt-2 underline underline-offset-2 inline-flex items-center gap-1 text-base text-base-content-secondary hover:text-primary transition-all delay-100"
-                >
-                  Details
-                  <ExternalLink className="w-4 h-4" />
-                </Link>
-              </div>
+              <Link href={project.url}>
+                <div key={idx} className="bg-transparent rounded-lg group">
+                  <div className="relative">
+                    <Image
+                      className="rounded-lg"
+                      src={project.image}
+                      alt={project.title}
+                    />
+                    <div className="group-hover:bg-black/50 absolute top-0 bottom-0 left-0 right-0 rounded-lg"></div>
+                    <Link
+                      href={project.url}
+                      className="hidden group-hover:inline-flex absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 primary-btn"
+                    >
+                      View Details
+                    </Link>
+                  </div>
+                  <h3 className="mt-3 group-hover:text-primary text-xl font-medium text-base-content-secondary">
+                    {project.title}
+                  </h3>
+                </div>
+              </Link>
             ))}
           </div>
         </div>
